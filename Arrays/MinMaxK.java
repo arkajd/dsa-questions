bdd75ffa0bf103b464d6567bd227381791f66953
arkajd <arkajd95@gmail.com>
2022-08-28 17:48:32 +0530

    find Kth min/max element

---
+++ Arrays/MinMaxK.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class MinMaxK {
+    
+    public static void main(String[] args) {
+        int[] arr = {5,2,3,4,-5};
+        int k = 1;
+        printKMin(arr, k);
+        printKMax(arr, k);
+    }
+
+    // print kth largest element in the array
+    private static void printKMax(int[] arr, int k){
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-k]);
+    }
+
+    // print kth smallest element in the array
+    private static void printKMin(int[] arr, int k){
+        Arrays.sort(arr);
+        System.out.println(arr[k-1]);
+    }
+    
+}
