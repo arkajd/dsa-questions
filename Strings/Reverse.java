6fa28c7a38a88e2a6359458d8cf74acf212876ec
arkajd <arkajd95@gmail.com>
2022-08-30 01:22:35 +0530

    add strings problems

---
+++ Strings/Reverse.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+        String str = "hello world";
+        String rev = reverseString(str);
+        System.out.println(rev);
+    }
+
+    private static String reverseString(String str){
+        StringBuilder sbl = new StringBuilder(str);
+        int left = 0;
+        int right = sbl.length()-1;
+
+        while(left<right){
+            char l = sbl.charAt(left);
+            char r = sbl.charAt(right);
+
+            sbl.setCharAt(left, r);
+            sbl.setCharAt(right, l);
+            left++;
+            right--;
+        }
+        return sbl.toString();
+    }
+    
+}
