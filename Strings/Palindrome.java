47f82665593393b9fbfe23de5bdae205bbc2c020
arkajd <arkajd95@gmail.com>
2022-08-30 01:29:58 +0530

    add code for palindrome

--- Strings/Palindrome.java
+++ Strings/Palindrome.java
@@ -1,5 +1,25 @@
 package Strings;
 
 public class Palindrome {
+
+    public static void main(String[] args) {
+        String str = "loyol loyal";
+        System.out.println(checkPalindrome(str));
+    }
+
+    private static boolean checkPalindrome(String str){
+
+        int left = 0;
+        int right = str.length()-1;
+
+        while(left<right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
     
 }
