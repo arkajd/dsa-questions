3ba9294b14d2ee6452e239e7dfb5dba838301e2d
arkajd <arkajd95@gmail.com>
2022-08-29 16:43:29 +0530

    move negatives to left

---
+++ Arrays/MoveNegatives.java
@@ -0,0 +1,38 @@
+public class MoveNegatives {
+    
+    public static void main(String[] args) {
+        int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+        int[] result = moveNegativesLeft(arr);
+        print(result);
+    }
+
+    private static int[] moveNegativesLeft(int[] arr){
+        int i = 0;
+        int j = arr.length -1;
+
+        while(i<j){
+            if(arr[i]>=0 && arr[j]<0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            else if(arr[i]>=0 && arr[j]>=0){
+                j--;
+            }
+            else if(arr[i]<0 && arr[j]<0){
+                i++;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return arr;
+    }
+
+    private static void print(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
